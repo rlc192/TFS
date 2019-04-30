966ddbfed8afc9ca40d9273f3adaaecaed8ba9c4
rlc192 <rlc192@builder.cs.rutgers.edu>
2019-04-29 21:23:24 -0400

    formatting

--- block.c
+++ block.c
@@ -1,6 +1,6 @@
 /*
  *  Copyright (C) 2019 CS416 Spring 2019
- *	
+ *
  *	Tiny File System
  *
  *	File:	block.c
@@ -26,59 +26,54 @@ int diskfile = -1;
 
 //Creates a file which is your new emulated disk
 void dev_init(const char* diskfile_path) {
-    if (diskfile >= 0) {
+	if (diskfile >= 0) {
 		return;
-    }
-    
-    diskfile = open(diskfile_path, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-    if (diskfile < 0) {
+	}
+	diskfile = open(diskfile_path, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+	if (diskfile < 0) {
 		perror("disk_open failed");
 		exit(EXIT_FAILURE);
-    }
-	
-    ftruncate(diskfile, DISK_SIZE);
+	}
+	ftruncate(diskfile, DISK_SIZE);
 }
 
 //Function to open the disk file
 int dev_open(const char* diskfile_path) {
-    if (diskfile >= 0) {
+	if (diskfile >= 0) {
 		return 0;
-    }
-    
-    diskfile = open(diskfile_path, O_RDWR, S_IRUSR | S_IWUSR);
-    if (diskfile < 0) {
+	}
+	diskfile = open(diskfile_path, O_RDWR, S_IRUSR | S_IWUSR);
+	if (diskfile < 0) {
 		perror("disk_open failed");
 		return -1;
-    }
+	}
 	return 0;
 }
 
 void dev_close() {
-    if (diskfile >= 0) {
+	if (diskfile >= 0) {
 		close(diskfile);
-    }
+	}
 }
 
 //Read a block from the disk
 int bio_read(const int block_num, void *buf) {
-    int retstat = 0;
-    retstat = pread(diskfile, buf, BLOCK_SIZE, block_num*BLOCK_SIZE);
-    if (retstat <= 0) {
+	int retstat = 0;
+	retstat = pread(diskfile, buf, BLOCK_SIZE, block_num*BLOCK_SIZE);
+	if (retstat <= 0) {
 		memset (buf, 0, BLOCK_SIZE);
-		if (retstat < 0)
-			perror("block_read failed");
-    }
-
-    return retstat;
+	if (retstat < 0)
+		perror("block_read failed");
+	}
+	return retstat;
 }
 
 //Write a block to the disk
 int bio_write(const int block_num, const void *buf) {
-    int retstat = 0;
-    retstat = pwrite(diskfile, buf, BLOCK_SIZE, block_num*BLOCK_SIZE);
-    if (retstat < 0) {
-		    perror("block_write failed");
-    }
-    return retstat;
+	int retstat = 0;
+	retstat = pwrite(diskfile, buf, BLOCK_SIZE, block_num*BLOCK_SIZE);
+	if (retstat < 0) {
+		perror("block_write failed");
+	}
+	return retstat;
 }
-
