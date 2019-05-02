f5f01107fc4f6231f5ce625efdab6c1e44fd3619
rlc192 <rlc192@kill.cs.rutgers.edu>
2019-05-02 00:35:16 -0400

    newest

--- tfs.c
+++ tfs.c
@@ -1,6 +1,6 @@
 /*
  *  Copyright (C) 2019 CS416 Spring 2019
- *	
+ *
  *	Tiny File System
  *
  *	File:	tfs.c
@@ -27,38 +27,39 @@
 #include "tfs.h"
 
 char diskfile_path[PATH_MAX];
+struct superblock sb;
 
 // Declare your in-memory data structures here
 
-/* 
+/*
  * Get available inode number from bitmap
  */
 int get_avail_ino() {
 
 	// Step 1: Read inode bitmap from disk
-	
+
 	// Step 2: Traverse inode bitmap to find an available slot
 
-	// Step 3: Update inode bitmap and write to disk 
+	// Step 3: Update inode bitmap and write to disk
 
 	return 0;
 }
 
-/* 
+/*
  * Get available data block number from bitmap
  */
 int get_avail_blkno() {
 
 	// Step 1: Read data block bitmap from disk
-	
+
 	// Step 2: Traverse data block bitmap to find an available slot
 
-	// Step 3: Update data block bitmap and write to disk 
+	// Step 3: Update data block bitmap and write to disk
 
 	return 0;
 }
 
-/* 
+/*
  * inode operations
  */
 int readi(uint16_t ino, struct inode *inode) {
@@ -75,16 +76,16 @@ int readi(uint16_t ino, struct inode *inode) {
 int writei(uint16_t ino, struct inode *inode) {
 
 	// Step 1: Get the block number where this inode resides on disk
-	
+
 	// Step 2: Get the offset in the block where this inode resides on disk
 
-	// Step 3: Write inode to disk 
+	// Step 3: Write inode to disk
 
 	return 0;
 }
 
 
-/* 
+/*
  * directory operations
  */
 int dir_find(uint16_t ino, const char *fname, size_t name_len, struct dirent *dirent) {
@@ -102,7 +103,7 @@ int dir_find(uint16_t ino, const char *fname, size_t name_len, struct dirent *di
 int dir_add(struct inode dir_inode, uint16_t f_ino, const char *fname, size_t name_len) {
 
 	// Step 1: Read dir_inode's data block and check each directory entry of dir_inode
-	
+
 	// Step 2: Check if fname (directory name) is already used in other entries
 
 	// Step 3: Add directory entry in dir_inode's data block and write to disk
@@ -119,7 +120,7 @@ int dir_add(struct inode dir_inode, uint16_t f_ino, const char *fname, size_t na
 int dir_remove(struct inode dir_inode, const char *fname, size_t name_len) {
 
 	// Step 1: Read dir_inode's data block and checks each directory entry of dir_inode
-	
+
 	// Step 2: Check if fname exist
 
 	// Step 3: If exist, then remove it from dir_inode's data block and write to disk
@@ -127,48 +128,83 @@ int dir_remove(struct inode dir_inode, const char *fname, size_t name_len) {
 	return 0;
 }
 
-/* 
+/*
  * namei operation
  */
 int get_node_by_path(const char *path, uint16_t ino, struct inode *inode) {
-	
+
 	// Step 1: Resolve the path name, walk through path, and finally, find its inode.
 	// Note: You could either implement it in a iterative way or recursive way
 
 	return 0;
 }
 
-/* 
+/*
  * Make file system
  */
 int tfs_mkfs() {
 
 	// Call dev_init() to initialize (Create) Diskfile
+	dev_init(diskfile_path);
 
 	// write superblock information
+	sb.magic_num = MAGIC_NUM;
+	sb.max_inum = MAX_INUM;
+	sb.max_dnum = MAX_DNUM;
+	sb.i_bitmap_blk = 1;
+	sb.d_bitmap_blk = 2;
+	sb.i_start_blk = 3;
+	sb.d_start_blk = MAX_INUM + 3;
+	char buff[BLOCK_SIZE];
+	memcpy(buff, &sb, sizeof(sb));
+	bio_write(0, buff);
 
 	// initialize inode bitmap
+	unsigned char inode_bitmap [MAX_INUM/8];
+	memset(inode_bitmap, 0, MAX_INUM/8);
+	// update bitmap information for root directory
+	inode_bitmap[0] |= (1 << 7);
+	bio_write(1, inode_bitmap);
 
 	// initialize data block bitmap
-
-	// update bitmap information for root directory
+	unsigned char data_bitmap [MAX_DNUM/8];
+	memset(data_bitmap, 0, MAX_DNUM/8);
+	bio_write(2, data_bitmap);
 
 	// update inode for root directory
+	struct inode root;
+	root.ino = 0;
+	root.valid = 1;
+	root.size = 0;
+	root.type = 1;
+	root.link = 0;
 
 	return 0;
 }
 
 
-/* 
+/*
  * FUSE file operations
  */
 static void *tfs_init(struct fuse_conn_info *conn) {
 
 	// Step 1a: If disk file is not found, call mkfs
+	if (access(diskfile_path, F_OK) == -1){	//diskfile does not exist
+		tfs_mkfs();
+	}
 
-  // Step 1b: If disk file is found, just initialize in-memory data structures
+	// Step 1b: If disk file is found, just initialize in-memory data structures
   // and read superblock from disk
-
+	else { //diskfile was found
+		dev_open(diskfile_path);
+		char buff [BLOCK_SIZE];
+		bio_read(0, (void*)buff);
+		memcpy(&sb, buff, sizeof(sb));
+		if (sb.magic_num != MAGIC_NUM){
+			fprintf(stderr, "ERROR:MAGIC_NUM DOES NOT MATCH\n");
+			exit(EXIT_FAILURE);
+		}
+	}
 	return NULL;
 }
 
@@ -225,7 +261,7 @@ static int tfs_mkdir(const char *path, mode_t mode) {
 	// Step 5: Update inode for target directory
 
 	// Step 6: Call writei() to write inode to disk
-	
+
 
 	return 0;
 }
@@ -380,4 +416,3 @@ int main(int argc, char *argv[]) {
 
 	return fuse_stat;
 }
-
